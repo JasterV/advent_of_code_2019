f23568a40cbae0c582a54baaf5660c8e7910f728
jasterv <victorcoder2@gmail.com>
2019-12-05 23:44:03 +0100

    spaghetti

--- day5/operations_mixin.dart
+++ day5/operations_mixin.dart
@@ -1,12 +1,24 @@
-
 const ADD = 1;
 const MULT = 2;
 const READ = 3;
 const SHOW = 4;
+const JUMP_IF_TRUE = 5;
+const JUMP_IF_FALSE = 6;
+const EQUAL_TO = 7;
+const LESS_THAN = 8;
 const HALT = 99;
 
-const Set operations = {ADD, MULT, READ, SHOW, HALT};
-
+const Set operations = {
+  ADD,
+  MULT,
+  READ,
+  SHOW,
+  JUMP_IF_TRUE,
+  JUMP_IF_FALSE,
+  EQUAL_TO,
+  LESS_THAN,
+  HALT,
+};
 
 const POSITION = 0;
 const IMMEDIATE = 1;
